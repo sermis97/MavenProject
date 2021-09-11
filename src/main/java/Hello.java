9b8a401e6a892acd0b2603e370906ccca833be35
sermis97 <sermis97@gmail.com>
2021-09-11 13:04:01 +0300

    fixed typed in the file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
         System.out.println("Developing hello feature");
         System.out.println("Developing hello feature");
         System.out.println("Local Repoo is already published to GitHub");
-        System.out.println("Doing more work adde");
+        System.out.println("Doing more work added");
     }
 }
